cc4a282b95557807dbd3f55622bf2498939f059d
Nikhil Raj <menikhilraj05@gmail.com>
2023-11-20 23:50:53 +0530

    Added Link instead of <a> tag in Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -13,22 +13,22 @@ function Nav(){
         <ul>
         <img src="" alt="logo"/>
         
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/menu">Menu</a></li>
-            <li><a href="/bookingpage">Reservations</a></li>
-            <li><a href="/order">Order Online</a></li>
-            <li><a href="/login">Login</a></li>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/menu">Menu</Link></li>
+            <li><Link to="/bookingpage">Reservations</Link></li>
+            <li><Link to="/order">Order Online</Link></li>
+            <li><Link to="/login">Login</Link></li>
         </ul>
         </nav>
 
 
-            <BrowserRouter>
+          
         <Routes> 
     <Route path="/" element={<Homepage />}></Route>
     <Route path="/bookingpage" element={<BookingPage/>}></Route>
 </Routes>
-</BrowserRouter>
+
        
         </>
     );
